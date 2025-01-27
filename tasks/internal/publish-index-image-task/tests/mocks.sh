e7f5a5868f24c9a6bea688029cd9440e7bde270d
Happy Bhati <hbhati@redhat.com>
2025-01-27 09:48:50 -0500

    feat(RELEASE-1245): make publish index image task idempotent
    Signed-off-by: Happy Bhati <hbhati@redhat.com>

--- tasks/internal/publish-index-image-task/tests/mocks.sh
+++ tasks/internal/publish-index-image-task/tests/mocks.sh
@@ -1,22 +1,31 @@
 #!/usr/bin/env bash
 set -x
 
-# mocks to be injected into task step scripts
-
 function skopeo() {
-  echo Mock skopeo called with: $* >&2
+  echo Mock skopeo called with: "$*" >&2
 
-  if [[ "$*" == *"--src-tls-verify=false --src-creds source docker://quay.io/source"* ]]
-  then
-    return 0
-  elif [[ "$*" == *"--src-tls-verify=false docker://registry-proxy.engineering.redhat.com/foo"* ]]
-  then
-    return 0
-  elif [[ "$*" == *"--src-tls-verify=false docker://registry-proxy.engineering.redhat.com/fail"* ]]
-  then
+  # Handle "skopeo inspect" for a failing case
+  if [[ "$*" == *"inspect --tls-verify=false docker://registry-proxy.engineering.redhat.com/fail --format '{{.Digest}}'"* ]]; then
+    echo "Mock failure: Image does not exist" >&2
     return 1
+
+  # Handle "skopeo inspect" for a successful case
+  elif [[ "$*" == *"inspect --tls-verify=false docker://registry-proxy.engineering.redhat.com/foo --format '{{.Digest}}'"* ]]; then
+    echo "sha256:abcd1234"
+    return 0
+
+  # Handle "skopeo inspect" for target image check
+  elif [[ "$*" == *"inspect --tls-verify=false docker://quay.io/target --format '{{.Digest}}'"* ]]; then
+    echo "sha256:xyz9876"
+    return 0
+
+  # Handle "skopeo copy"
+  elif [[ "$*" == *"skopeo copy"* ]]; then
+    echo "Mock skopeo copy called with: $*"
+    return 0  
+
   else
-    echo Error: Unexpected call
+    echo "Error: Unexpected call"
     exit 1
   fi
 }
